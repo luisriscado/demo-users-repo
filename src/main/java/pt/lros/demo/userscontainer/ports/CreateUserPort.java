692d634ca01a872290384d88d29c1ed61b7fb6ea
Luis Riscado <riscado_luis@msn.com>
2019-08-31 15:24:15 +0100

    setup ports for domain;

---
+++ src/main/java/pt/lros/demo/userscontainer/ports/CreateUserPort.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pt.lros.demo.userscontainer.ports;
+
+import pt.lros.demo.userscontainer.User;
+
+/**
+ *
+ * @author risca
+ */
+public interface CreateUserPort {
+
+    void create(User u);
+
+}
---
+++ src/main/java/pt/lros/demo/userscontainer/ports/ListUsersPort.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pt.lros.demo.userscontainer.ports;
+
+import java.util.List;
+import pt.lros.demo.userscontainer.User;
+
+/**
+ *
+ * @author risca
+ */
+public interface ListUsersPort {
+
+    List<User> listUsers();
+}
